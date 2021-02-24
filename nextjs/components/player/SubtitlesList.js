d3b00fcdff58791a9a9b243a89bfd2f04ead103e
Seyyed Hassan Asheghi <asheghi.sh@gmail.com>
2021-02-24 21:09:01 +0330

    fixed text prettier

--- nextjs/components/player/SubtitlesList.js
+++ nextjs/components/player/SubtitlesList.js
@@ -1,33 +1,50 @@
 export default function SubtitleList(props) {
-    const {seekTo, subtitles, onItemClick, onItemDoubleClick, currentMs} = props;
-    const {skipSilence} = props;
+  const {
+    seekTo,
+    subtitles,
+    onItemClick,
+    onItemDoubleClick,
+    currentMs,
+  } = props;
+  const { skipSilence } = props;
 
-    const indexActiveSub = subtitles.findIndex(it => it.active);
-    if (skipSilence && indexActiveSub < 0) {
-        const next = subtitles.find(it => it.start > currentMs);
-        if (next && next.start - currentMs > 500) {
-            seekTo(next.start / 1000)
-        }
+  const indexActiveSub = subtitles.findIndex((it) => it.active);
+  if (skipSilence && indexActiveSub < 0) {
+    const next = subtitles.find((it) => it.start > currentMs);
+    if (next && next.start - currentMs > 500) {
+      seekTo(next.start / 1000);
     }
+  }
 
-    function formatText (text){
-        return String(text).replaceAll('<i>','').replaceAll('</i>','');
-    }
+  function formatText(text) {
+    return String(text)
+      .replace(/<[^>]*>/g, "")
+      .replace(/=+/g, "");
+  }
 
-    return (<div>
-        {
-            subtitles
-                .map(it => {
-                    const rowStyle = 'row_common';
-                    const activeStyle = 'row_active';
-                    const inActiveStyle = 'row_in_active';
-                    return <div key={it.text + it.start + it.end}
-                                onDoubleClick={() => onItemDoubleClick(it)}
-                                onClick={() => onItemClick(it)}
-                                className={'row_common' + '  ' + 'noselect' + ' ' + (it.active ? activeStyle : inActiveStyle)}>
-                        {formatText(it.text)}
-                    </div>;
-                })
-        }
-    </div>)
+  return (
+    <div>
+      {subtitles.map((it) => {
+        const rowStyle = "row_common";
+        const activeStyle = "row_active";
+        const inActiveStyle = "row_in_active";
+        return (
+          <div
+            key={it.text + it.start + it.end}
+            onDoubleClick={() => onItemDoubleClick(it)}
+            onClick={() => onItemClick(it)}
+            className={
+              "row_common" +
+              "  " +
+              "noselect" +
+              " " +
+              (it.active ? activeStyle : inActiveStyle)
+            }
+          >
+            {formatText(it.text)}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
